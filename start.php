7ea5e217009b9ce7e6d4defcc55762ae0e6dab5b
hundenmarvin <hundenmarvin@gmail.com>
2015-02-12 17:29:50 +0100

    start.php o style.css: formaterade text i navigering o startsida

--- start.php
+++ start.php
@@ -10,22 +10,22 @@
 </div>
 <div id="main">
 	<div id="vision">
-		<h1>VÅR VISION</h1>
+		<h1>vår vision</h1>
 		<p>WaterWeave vill göra skillnad för miljön och öka tillgången på rent dagvatten. WaterWeave gör detta genom att utveckla smarta textilier för dagvattenrening. Genom att kombinera biomassa med textil möjliggör tekniken effektivt upptag av tungmetalljoner i vatten.</p>
 	</div>
 	<div id="waterweave">
 		<img src="img/1_s.png" class="col_image">
 		<div class="coltext">
-			<h1>WATERWEAVE</h1>
-			<p>WaterWeave är ett Göteborgsbaserat start-up som utvecklar ett textilbaserad vattenreningssystem för rening av dagvatten från tungmetalljoner.</p>
+			<h1>waterweave</h1>
+			<p>WaterWeave är ett Göteborgsbaserat<br> start-up som utvecklar ett textilbaserad<br> vattenreningssystem för rening av<br> dagvatten.</p>
 		</div>
 		<a href="#"><div class="button"><p class="readmore">Läs mer</p></div></a>
 	</div>
 	<div id="tekniken">
 		<img src="img/2_s.png" class="col_image">
 		<div class="coltext">
-			<h1>TEKNIKEN</h1>
-			<p>WaterWeave arbetar med att ta fram ett textilbaserat vattenreningssystem som bygger på biologisk rening, en ide som ursprungligen kommer från Textilhögskolan i Borås.</p>
+			<h1>tekniken</h1>
+			<p>WaterWeave arbetar med att ta fram ett<br> textilbaserat vattenreningssystem som<br> bygger på biologisk rening, en ide som<br> ursprungligen kommer från<br> Textilhögskolan i Borås.</p>
 		</div>
 		<a href="#"><div class="button"><p class="readmore">Läs mer</p></div></a>
 	</div>
