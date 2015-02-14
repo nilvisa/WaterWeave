c0e00e0a4fa18935512ea01eb3599fed29166ac9
Lovisa Nilsson <lovisa.petit@gmail.com>
2015-02-14 17:41:12 +0100

    grejar med inputs

--- adduser.php
+++ adduser.php
@@ -25,34 +25,37 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 ?>
 
 <html>
+<div id="main_admin">
 
-	<form method="post" enctype="multipart/form-data">
-		<input type="text" name="f_name" placeholder="Förnamn" 
-		value="<?php if(isset($_POST['f_name'])){ echo htmlentities($_POST['f_name']);} ?>"><br>
+	<div id="formdiv">
+		<h1>lägg till en ny i teamet</h1>
 
-		<input type="text" name="l_name" placeholder="Efternamn"
-		value="<?php if(isset($_POST['l_name'])){ echo htmlentities($_POST['l_name']);} ?>"><br>
+		<form method="post" enctype="multipart/form-data">
+			<input type="text" name="f_name" placeholder="Förnamn" 
+			value="<?php if(isset($_POST['f_name'])){ echo htmlentities($_POST['f_name']);} ?>"><br>
 
-		<input type="text" name="email" placeholder="E-mail"
-		value="<?php if(isset($_POST['email'])){ echo htmlentities($_POST['email']);} ?>"><br>
+			<input type="text" name="l_name" placeholder="Efternamn"
+			value="<?php if(isset($_POST['l_name'])){ echo htmlentities($_POST['l_name']);} ?>"><br>
 
-		<input type="text" name="tel" placeholder="Telefonnummer"
-		value="<?php if(isset($_POST['tel'])){ echo htmlentities($_POST['tel']);} ?>"><br>
+			<input type="text" name="email" placeholder="E-mail"
+			value="<?php if(isset($_POST['email'])){ echo htmlentities($_POST['email']);} ?>"><br>
 
-		<input type="text" name="title" placeholder="Titel"
-		value="<?php if(isset($_POST['title'])){ echo htmlentities($_POST['title']);} ?>"><br>
+			<input type="text" name="tel" placeholder="Telefonnummer"
+			value="<?php if(isset($_POST['tel'])){ echo htmlentities($_POST['tel']);} ?>"><br>
 
-		<textarea name="story" placeholder="Beskrivning" rows="5" cols="22"><?php if(isset($_POST['story'])){ echo htmlentities($_POST['story']);} ?></textarea><br>
+			<input type="text" name="title" placeholder="Titel"
+			value="<?php if(isset($_POST['title'])){ echo htmlentities($_POST['title']);} ?>"><br>
 
-		<input type="file" name="user_pic">	Välj en bild (valfritt)<br>
-		
-		<br><br>
-		<input type="submit" name="addPerson" value="Lägg till">
-	</form>
-	<br><br>
+			<textarea name="story" placeholder="Beskrivning" rows="5" cols="22"><?php if(isset($_POST['story'])){ echo htmlentities($_POST['story']);} ?></textarea><br>
 
-	<hr>
-
+			<input type="file" name="user_pic" value="Välj en bild (valfritt)">	<br>
+			
+			<br><br>
+			<input type="submit" name="addPerson" value="Lägg till" class="button">
+		</form>
+	</div>
+	
+</div>
 </html>
 
 <?php
