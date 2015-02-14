b41425ffe140b5a1ce599bfa205658c331a5607b
hundenmarvin <hundenmarvin@gmail.com>
2015-02-14 17:39:02 +0100

    teknik-sidan klar med basic css

--- tekniken.php
+++ tekniken.php
@@ -1,7 +1,38 @@
 <?php
 
 include('header.php');
+?>
 
+<div id="headerimg"><img src="img/head/4_h.png"></div>
+<div class="wrap">
+	<div id="main">
+		<h1>tekniken</h1>
+		<div class="wwtext">
+			<p>
+			Förorenat vatten med höga halter av tungmetaller är ett stort problem både på regional och global nivå. WaterWeave arbetar med att ta fram ett textilbaserat vattenreningssystem som bygger på biologisk rening, en ide som ursprungligen kommer från Textilhögskolan i Borås. Textilen kan bland annat användas i dagvattendammar, dagvattenbrunnar och i vägdiken för effektivt upptag av tungmetaller.
+			</p>
+			<p>
+			Det unika med denna lösning är att tekniken kombinerar biologisk rening med ett flexibelt material så som textil. Att arbeta med textil möjliggör att man kommer nära själv utsläppskällan eftersom det är enkelt att skapa en produkt som är anpassad efter kundens behov. Genom att angripa problemet tidigt i utsläppskedjan kan man uppnå en storskalig effekt. 
+			</p>
+		</div>
+		<div id="illustration">
+			<img src="img/7_s.png">
+		</div>
+		<div class="wwtext">
+			<p>
+			Till skillnad från ett vanligt filter behöver inte vattnet silas genom textilen, istället räcker det att vattnet kommer i kontakt med den aktiva ytan vilket öppnar upp för rening av stora mängder vatten.  Ett poröst material så som textil innebär stor verksam yta vilket också är viktigt för effektiv upptagning av tungmetalljoner.
+			</p>
+		</div>
+		<div class="teknik_img">
+			<img src="img/8_s.png">
+			<img src="img/IMG_9114.jpg"><!-- motsvarande returscherad bild saknas -->
+		</div>
+		
+
+	</div>
+</div>
+
+<?php
 include('footer.php');
 
 ?>
