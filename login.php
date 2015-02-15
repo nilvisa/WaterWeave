6f8502d86c02d25d1e88df4ca1ad56b9947ff638
hundenmarvin <hundenmarvin@gmail.com>
2015-02-15 15:01:39 +0100

    arbetade med login-sida

--- login.php
+++ login.php
@@ -1,10 +1,42 @@
 <?php
+$ttl = 10 * 60;
+session_set_cookie_params($ttl);
 session_start();
 
 
 
-?>
+include("server/funcs.php");
 
+include("server/adminprofil.php");
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+	$myusername = $_POST['myusername'];
+	$mypassword = $_POST['mypassword'];
+
+
+
+	if($myusername == $username && $mypassword == $password){
+
+
+		$_SESSION["login"] = true;
+		header("location:adminstart.php");
+		setcookie(session_name(), session_id(), time() + $ttl);
+
+
+	}
+	else {
+		echo "Fel användarnamn eller lösenord";
+	}
+
+
+
+
+}
+
+?>
 
 
 <!DOCTYPE html>
@@ -12,7 +44,6 @@ session_start();
 <head>
 	<meta charset="UTF-8">
 	<title>WaterWeave</title>
-	<!--<link href="css/reset.css" type="text/css" rel="stylesheet" />-->
 	<link href="css/style.css" type="text/css" rel="stylesheet" />
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700,400' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Vollkorn:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
@@ -21,16 +52,16 @@ session_start();
 <body>
 	
 
-<html>
+<body>
 	<div id="formdiv">
 		<h1>logga in som admin</h1>
 
 		<form action="login.php" method="post">
 			<div class="inputdiv">
-				<input type="text" name="username" placeholder="Användarnamn">
+				<input type="text" name="myusername" placeholder="Användarnamn">
 			</div>
 			<div class="inputdiv">
-				<input type="password" size="25" name="pass" placeholder="Lösenord">
+				<input type="password" size="25" name="mypassword" placeholder="Lösenord">
 			</div>
 			<div style="clear: both"></div>
 
