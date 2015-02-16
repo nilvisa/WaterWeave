d4d58929b5d43e29b8cd93966425d1a9e6395ffc
hundenmarvin <hundenmarvin@gmail.com>
2015-02-16 19:18:00 +0100

    mail form

--- kontakt.php
+++ kontakt.php
@@ -1,10 +1,43 @@
 <?php
 
-include('header.php');
+//include('header.php');
 
-$sandra = getPerson('13');
-$maria = getPerson('12');
+//$sandra = getPerson('13');
+//$maria = getPerson('12');
+
+
+
+if(isset($_POST["submit"])){
+// Checking For Blank Fields..
+if($_POST["vname"]==""||$_POST["vemail"]==""||$_POST["sub"]==""||$_POST["msg"]==""){
+echo "Fill All Fields..";
+}else{
+// Check if the "Sender's Email" input field is filled out
+$email=$_POST['vemail'];
+// Sanitize E-mail Address
+$email =filter_var($email, FILTER_SANITIZE_EMAIL);
+// Validate E-mail Address
+$email= filter_var($email, FILTER_VALIDATE_EMAIL);
+if (!$email){
+echo "Invalid Sender's Email";
+}
+else{
+$subject = $_POST['sub'];
+$message = $_POST['msg'];
+$headers = 'From:'. $email . "\r\n"; // Sender's Email
+$headers .= 'Cc:'. $email . "\r\n"; // Carbon copy to Sender
+// Message lines should not exceed 70 characters (PHP rule), so wrap it
+$message = wordwrap($message, 70);
+// Send Mail By PHP Mail Function
+mail("hundenmarvin@gmail.com", $subject, $message, $headers);
+echo "Your mail has been sent successfuly ! Thank you for your feedback";
+}
+}
+}
+ 
 ?>
+
+
 
 <div id="headerimg"><img src="img/head/4_h.png"></div>
 <div class="wrap">
@@ -15,29 +48,29 @@ $maria = getPerson('12');
 			<h3>Adress</h3>
 			<p>Vera Sandbergsallé 8</br>412 96 Göteborg</p>
 			<h3>Sandra Melin</h3>
-			<p><?php print $sandra['tel']; ?></br>sandra@waterweave.se</p>
+			<p><?php //print $sandra['tel']; ?></br>sandra@waterweave.se</p>
 			<h3>Maria Knutsson</h3>
-			<p><?php print $maria['tel']; ?></br>maria@waterweave.se</p>
+			<p><?php //print $maria['tel']; ?></br>maria@waterweave.se</p>
 			</div>
 			<img id="kontaktimg" src="img/14_s-png.png">
 		</div>
-		<form action="kontakt.php" method="post">
+		<form action="kontakt.php" method="post" name="form">
 			<div class="inputdiv">
-				<input type="text" name="subject" placeholder="Ämne">
+				<input type="text" name="vname" placeholder="Namn">
 			</div>
 			<div class="inputdiv">
-				<input type="text" name="mail" placeholder="E-mail">
+				<input type="text" name="vemail" placeholder="E-mail">
 			</div>
 			<div class="inputdiv">
-				<input type="text" name="phone" placeholder="Telefonnummer">
+				<input type="text" name="sub" placeholder="Ämne">
 			</div>
 			<div class="inputdiv">
-				<input type="text" name="content" placeholder="Ärende">
+				<textarea name="msg" placeholder="Skriv din text här..."></textarea>
 			</div>
 			<div style="clear: both"></div>
 
 			<br>
-			<input type="submit" value="skicka" class="button">
+			<input type="submit" value="skicka" class="button" name="submit">
 	
 		</form>
 
@@ -50,6 +83,6 @@ $maria = getPerson('12');
 
 
 <?php
-include('footer.php');
+//include('footer.php');
 
 ?>
