18a1e267141678d348303c5e6fa853578648cec7
hundenmarvin <hundenmarvin@gmail.com>
2015-02-16 20:24:09 +0100

    kontakt finslip

--- kontakt.php
+++ kontakt.php
@@ -1,16 +1,17 @@
 <?php
-
 include('header.php');
 
 $sandra = getPerson('13');
 $maria = getPerson('12');
 
 
+
 //följande kod är tagen från denna sida: http://www.formget.com/send-an-email-on-form-submission-using-php/
+$errormsg = "";
 if(isset($_POST["submit"])){
 // Checking For Blank Fields..
 if($_POST["vname"]==""||$_POST["vemail"]==""||$_POST["sub"]==""||$_POST["msg"]==""){
-echo "Fyll i alla fält..";
+$errormsg = "Fyll i alla fält..";
 }else{
 // Check if the "Sender's Email" input field is filled out
 $email=$_POST['vemail'];
@@ -19,7 +20,7 @@ $email =filter_var($email, FILTER_SANITIZE_EMAIL);
 // Validate E-mail Address
 $email= filter_var($email, FILTER_VALIDATE_EMAIL);
 if (!$email){
-echo "Avsändarens e-mail är ogiltig";
+$errormsg = "Avsändarens e-mail är ogiltig";
 }
 else{
 $subject = $_POST['sub'];
@@ -30,7 +31,7 @@ $headers .= 'Cc:'. $email . "\r\n"; // Carbon copy to Sender
 $message = wordwrap($message, 70);
 // Send Mail By PHP Mail Function
 mail("hundenmarvin@gmail.com", $subject, $message, $headers);
-echo "Ditt meddelande är skickat! Tack för din feedback";
+$errormsg = "Ditt meddelande är skickat! Tack för din feedback";
 }
 }
 }
@@ -42,18 +43,21 @@ echo "Ditt meddelande är skickat! Tack för din feedback";
 <div id="headerimg"><img src="img/head/4_h.png"></div>
 <div class="wrap">
 	<div id="main">
+
 		<h1>kontakt</h1>
 		<div class="kontakt2col">
 			<div id="kontaktadresser">
-			<h3>Adress</h3>
-			<p>Vera Sandbergsallé 8</br>412 96 Göteborg</p>
-			<h3>Sandra Melin</h3>
-			<p><?php print $sandra['tel']; ?></br>sandra@waterweave.se</p>
-			<h3>Maria Knutsson</h3>
-			<p><?php print $maria['tel']; ?></br>maria@waterweave.se</p>
+				<h3>Adress</h3>
+				<p>Vera Sandbergsallé 8</br>412 96 Göteborg</p>
+				<h3>Sandra Melin</h3>
+				<p><?php print $sandra['tel']; ?></br>sandra@waterweave.se</p>
+				<h3>Maria Knutsson</h3>
+				<p><?php print $maria['tel']; ?></br>maria@waterweave.se</p>
 			</div>
 			<img id="kontaktimg" src="img/14_s-png.png">
 		</div>
+
+		<p><?php print $errormsg; ?></p>
 		<form action="kontakt.php" method="post" name="form">
 			<div class="inputdiv">
 				<input type="text" name="vname" placeholder="Namn">
@@ -68,10 +72,8 @@ echo "Ditt meddelande är skickat! Tack för din feedback";
 				<textarea name="msg" placeholder="Skriv din text här..."></textarea>
 			</div>
 			<div style="clear: both"></div>
-
 			<br>
 			<input type="submit" value="skicka" class="button" name="submit">
-	
 		</form>
 
 	</div>
@@ -79,10 +81,6 @@ echo "Ditt meddelande är skickat! Tack för din feedback";
 
 
 
-
-
-
 <?php
 include('footer.php');
-
 ?>
