1a54b56e7cd81f1010131698493cbf94387b4cfb
Lovisa Nilsson <lovisa.petit@gmail.com>
2015-02-17 02:11:18 +0100

    fixat admin headern

--- adminheader.php
+++ adminheader.php
@@ -24,19 +24,23 @@ include("server/funcs.php");
 	<meta charset="UTF-8">
 	<title>WaterWeave - Admin</title>
 	<link href="css/style.css" type="text/css" rel="stylesheet" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700,400' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Vollkorn:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
 	<script src="js/jquery-1.11.2.min.js"></script>
 </head>
 <body>
 
 <div id="header">
+
 	<div id="headermain">
 		<a href="index.php">
 			<img src="img/head/logotyp_vector.png" id="logga">
 		</a>
-		<nav>
-			<ul>
+	</div>
+</div>
+
+<div id="stop">
+	<div id="stopmain">
+	<nav>
+		<ul>
 				<li><a href="addnews.php">ny nyhet</a></li>
 				<li><a href="editnews.php">redigera nyheter</a></li>
 				<li>|</li>
@@ -44,14 +48,18 @@ include("server/funcs.php");
 				<li><a href="edituser.php">redigera teamet</a></li>
 			</ul>
 		</nav>
+
+		<div id="logout">
+			<nav>
+			<li><a href="logout.php">logga ut</a></li>
+			</nav>
+		</div>
 	</div>
-	<div id="logout">
-		<nav>
-		<li><a href="logout.php">logga ut</a></li>
-		</nav>
-	</div>
-	<div id="border"></div>
-</div>
+
+
+	<div id="border"></div>	
+
+</div>	
 
 <script>
 
@@ -61,6 +69,18 @@ $(document).ready(function() {
         $(this).addClass("currentLink");
         }
     });
+});
+
+$(window).scroll(function () {
+    var height = $('body').height();
+    var scrollTop = $(window).scrollTop();
+
+      if(scrollTop>55){
+          $('#stop').css({ 'position': 'fixed', 'top': '0px', 'background': '#fff'});
+    	}
+      else{
+         $('#stop').css({ 'position': 'absolute', 'top': '55px', 'background': 'none'});
+       }
 });
 
 </script>
