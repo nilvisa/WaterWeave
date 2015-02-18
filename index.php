6138a3fd0d4a9895b2e2db801cbb4d67d8f8b72f
Lovisa Nilsson <lovisa.petit@gmail.com>
2015-02-18 22:17:55 +0100

    responsivt

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php include_once("header.php"); ?>
+<?php include_once("header.php"); 
+
+$sandra = getPerson('13');
+$maria = getPerson('12');
+
+include('server/adminprofil.php'); //För att få mail-adressen som formuläret postar till.
+
+?>
 
 <div class="stor_wrap vit">
 	<img src="img/head/1_h2.png" id="imgflow">
@@ -42,6 +49,51 @@
 	</div>
 
 	<div class="clearfix"></div>
+
+	<div id="start_kontakt">
+
+		<h2>kontakt</h2>
+
+			<br><br>
+			
+			<h3>adress</h3>
+			<p>Vera Sandbergsallé 8</br>412 96 Göteborg</p>
+			<br><br>
+
+			<h3>sandra melin</h3>
+			<p><?php print $sandra['tel']; ?></br>sandra@waterweave.se</p>
+			<br><br>
+
+			<h3>maria knutsson</h3>
+			<p><?php print $maria['tel']; ?></br>maria@waterweave.se</p>
+
+		
+<br><br>
+
+		<div id="kontaktform">
+			<form action="http://formmail.loopia-waterweave.se" method="post" name="form">
+				<input type="hidden" name="recipient" value="<?php print $yourmail; ?>">
+				<input type="hidden" name="redirect" 
+  					value="mailsubmit.php">
+				<div class="inputdiv">
+					<input type="text" name="name" placeholder="Namn">
+				</div>
+				<div class="inputdiv">
+					<input type="text" name="email" placeholder="E-mail">
+				</div>
+				<div class="inputdiv">
+					<input type="text" name="subject" placeholder="Ämne">
+				</div>
+				<div class="inputdiv">
+					<textarea name="body" placeholder="Skriv din text här..." rows="5"></textarea>
+				</div>
+				<div style="clear: both"></div>
+				<br>
+				<input type="submit" value="skicka" class="button" name="submit">
+			</form>
+		</div>
+
+	</div>
 
 </div>
 	<div class="wrap vit">
